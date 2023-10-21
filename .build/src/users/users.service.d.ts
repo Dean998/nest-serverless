4f53595d1f99e7593202443c3109a60d1a359983
Dean <dean.b.98@hotmail.com>
2023-10-21 23:53:21 +0100

    Removed build directory from tracking and added to .gitignore

--- .build/src/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../../prisma';
-import { Bookings } from '@prisma/client';
-export declare class UsersService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createUser(id: number, name: string, date: string): Promise<Bookings>;
-    findAll(): Promise<Bookings[]>;
-}
